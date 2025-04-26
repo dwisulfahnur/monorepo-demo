fdbb3e371da7fb7003ea43d8ec326bc2759ca401
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-04-26 14:52:01 +0700

    feat: add firebase configuration

---
+++ apps/backend-repo/src/config/firebaseConfig.ts
@@ -0,0 +1,15 @@
+import admin from 'firebase-admin'
+import path from 'path'
+
+
+if (!admin.apps.length) {
+  const serviceAccountPath = path.resolve(__dirname, '../core/secrets/firebase.json');
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountPath),
+  });
+}
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+export { db, auth }
