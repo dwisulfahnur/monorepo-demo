8c2dc0cbee6696bf86ac1032a0f64cc08af7eb32
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-04-26 15:20:49 +0700

    feat: create new middleware: authMiddleware

---
+++ apps/backend-repo/src/middleware/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { auth } from "../config/firebaseConfig";
+
+const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    res.sendStatus(401);
+    return;
+  }
+
+  try {
+    const decodedToken = await auth.verifyIdToken(token);
+    if (!decodedToken.uid) {
+      res.sendStatus(403);
+      return;
+    }
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(403).send(error);
+  }
+}
+
+export default authMiddleware;
