fdabd22b7495f94591510c5593395e71d757fd07
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-04-28 23:35:50 +0700

    feat: implement dotenv, then cors and express json to the express app instance

--- apps/backend-repo/src/core/app.ts
+++ apps/backend-repo/src/core/app.ts
@@ -1,15 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+import cors from "cors";
 import express from 'express';
 import userRoutes from '../routes/userRoutes';
 import authMiddleware from '../middleware/authMiddleware';
 
 
 const app = express();
-const PORT = 4000;
 
+app.use(cors())
+app.use(express.json())
 app.use(authMiddleware);
-app.use('', userRoutes)
-app.listen(PORT, () => {
-    console.log(`Backend listening on http://localhost:${PORT}`);
-});
-
+app.use(userRoutes)
 export default app;
