c597467f4f79bc01d2658dd924115f6976f28842
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-05-01 03:04:11 +0700

    feat: update homepage to be using HomeTemplate and UserProfile as the content

--- apps/frontend-repo/src/app/page.tsx
+++ apps/frontend-repo/src/app/page.tsx
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { auth } from "@/lib/firebase";
+import { useEffect, useState } from "react";
+import { UserProfile } from "@/components/organisms/user-profile";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/hooks";
+import { onAuthStateChanged } from "firebase/auth";
+import { store } from "@/store";
+import { setUser } from "@/store/slices/authSlice";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress"
+import HomeTemplate from "@/components/templates/home-template";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter()
+  const { user } = useAuthStore()
+  const [isLoading, setIsLoading] = useState(true)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    onAuthStateChanged(auth, (user_) => {
+      if (user_) {
+        store.dispatch(setUser(user_))
+      } else {
+        router.replace('/signin')
+      }
+      setIsLoading(false)
+    })
+  }, [])
+
+  const cardVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  }
+
+  if (isLoading) {
+    return (
+      <Box sx={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <CircularProgress size={40} />
+      </Box>
+    )
+  }
+
+  return (
+    <HomeTemplate>
+      {user ? (
+        <motion.div variants={cardVariants} initial="hidden" animate="visible">
+          <UserProfile user={user} />
+        </motion.div>
+      ) : null}
+    </HomeTemplate>
   );
 }
