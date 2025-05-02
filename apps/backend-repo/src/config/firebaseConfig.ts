bef2be857d395976265ffe75300e6c6164c24c28
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-05-02 15:16:20 +0700

    feat: handle error if the firebase config file is not provided

--- apps/backend-repo/src/config/firebaseConfig.ts
+++ apps/backend-repo/src/config/firebaseConfig.ts
@@ -3,17 +3,24 @@ import path from 'path'
 
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.resolve(__dirname, '../config/secrets/firebase.json');
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
-  });
+  try {
+    const serviceAccountPath = path.resolve(__dirname, '../config/secrets/firebase.json');
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccountPath),
+    });
+  } catch (err) {
+    console.log("Failed to load firebase configuration file, initializa app without credentials!")
+
+    // initialize app with default projectId
+    admin.initializeApp({ projectId: 'ebuddy-test-3abe2' })
+  }
 }
 
 const db = admin.firestore();
 const auth = admin.auth();
 
 if (process.env.FIRESTORE_EMULATOR_HOST) {
-  db.settings({host: process.env.FIRESTORE_EMULATOR_HOST})
+  db.settings({ host: process.env.FIRESTORE_EMULATOR_HOST })
 }
 
 export { db, auth }
